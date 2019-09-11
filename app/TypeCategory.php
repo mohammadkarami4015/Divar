67c5a3689c1fafdd89e32ba75107c5f8766e590a
unknown <mohammadkarami0815@gmail.com>
2019-09-11 10:25:00 +0430

    'create_models'

---
+++ app/TypeCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TypeCategory extends Model
+{
+    protected $table = 'typecategory';
+    protected $guarded = [];
+    public $timestamps = false;
+}
